7e447fad889b9c7debfa208d3c429e68f79094d1
Ciro Mattia Gonano <ciromattia@gmail.com>
2014-05-03 14:59:16 +0200

    Fix Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -393,23 +393,7 @@ module.exports = function (grunt) {
       }
 		},
 
-    coveralls: {
-      options: {
-        // LCOV coverage file relevant to every target
-        src: 'coverage-results/lcov.info',
-
-        // When true, grunt-coveralls will only print a warning rather than
-        // an error, to prevent CI builds from failing unnecessarily (e.g. if
-        // coveralls.io is down). Optional, defaults to false.
-        force: false
-      },
-      your_target: {
-        // Target-specific LCOV coverage file
-        src: 'coverage-results/extra-results-*.info'
-      }
-    },
-
-		protractor: {
+    		protractor: {
 			options: {
 				keepAlive: true,
 				configFile: 'test/protractor.conf.js'
