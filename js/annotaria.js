0ee322cb4c69429f4400b97d0b7272d611619291
Ciro Mattia Gonano <ciromattia@gmail.com>
2014-05-23 12:12:45 +0200

    AngularJs no more :(

---
+++ js/annotaria.js
@@ -0,0 +1,33 @@
+'use strict';
+
+$(document).ready(main);
+
+function main() {
+	$.ajax({
+		method: 'GET',
+		url: 'http://localhost/annotaria-td/articlelist',
+		success: function (d) {
+			for (var i = 0; i < d.length; i++) {
+				$('#articlelist').append('<li class="nav-index-listing">' +
+				'<small><a href="#" onclick="load(\'' + d[i].href + '\')">' +
+				d[i].title + "</a></li>");
+			}
+		},
+		error: function (a, b, c) {
+			alert('No documents.')
+		}
+	});
+}
+
+function load(file) {
+	$.ajax({
+		method: 'GET',
+		url: 'http://localhost/annotaria-td/article/' + file,
+		success: function (d) {
+			$('#current_article').html(d)
+		},
+		error: function (a, b, c) {
+			alert('Cannot load file ' + file)
+		}
+	});
+}
